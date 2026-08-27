bac6ad10d3b64c0d96bd29a8d9f2ec725462a9f5
218218y <218yaakov@gmail.com>
2026-08-27 22:03:01 +0300

    ניקוי כיתוב 2

--- netunim-orders/site/service-worker.js
+++ netunim-orders/site/service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 const CACHE_PREFIX='orders-app-shell-';
-const CACHE='orders-app-shell-esm-6798f37fee82';
+const CACHE='orders-app-shell-esm-6eaaf9632b53';
 const SHELL=[
   './',
   './index.html',
