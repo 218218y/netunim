afd42a8cb926d7c5af1ab7118428dec18a56c107
218218y <218yaakov@gmail.com>
2026-08-28 05:05:23 +0300

    קביעת טאבים -1

--- netunim-orders/site/service-worker.js
+++ netunim-orders/site/service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 const CACHE_PREFIX='orders-app-shell-';
-const CACHE='orders-app-shell-esm-bc7f3a6119ca';
+const CACHE='orders-app-shell-esm-0ed557bdf7a0';
 const SHELL=[
   './',
   './index.html',
