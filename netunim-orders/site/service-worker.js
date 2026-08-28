fc43fc9547fc99429e7085a2320fa7189245b042
218218y <218yaakov@gmail.com>
2026-08-28 06:54:18 +0300

    הקטנת שדה סכום לקוחות

--- netunim-orders/site/service-worker.js
+++ netunim-orders/site/service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 const CACHE_PREFIX='orders-app-shell-';
-const CACHE='orders-app-shell-esm-b51a77f93278';
+const CACHE='orders-app-shell-esm-f460cbb92316';
 const SHELL=[
   './',
   './index.html',
