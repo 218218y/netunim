757f6ae7f74379c6b603bdd7d03bc12495cf56be
218218y <218yaakov@gmail.com>
2026-08-28 16:13:34 +0300

    Update service-worker.js

--- netunim-orders/site/service-worker.js
+++ netunim-orders/site/service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 const CACHE_PREFIX='orders-app-shell-';
-const CACHE='orders-app-shell-esm-af5529325d97';
+const CACHE='orders-app-shell-esm-2dc39245f681';
 const SHELL=[
   './',
   './index.html',
