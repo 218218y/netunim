9a740f508e769d983b0a0e31c9156002bbfcbc41
218218y <218yaakov@gmail.com>
2026-09-01 22:06:58 +0300

    שיפור יתרה למשיכה

--- netunim-orders/site/service-worker.js
+++ netunim-orders/site/service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 const CACHE_PREFIX='orders-app-shell-';
-const CACHE='orders-app-shell-esm-f1d40ac70cb9';
+const CACHE='orders-app-shell-esm-2ec3dab2b547';
 const SHELL=[
   './',
   './index.html',
