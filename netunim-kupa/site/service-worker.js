d043f81bf1d613560f4e31964acf8d797a40c897
218218y <218yaakov@gmail.com>
2026-09-08 06:22:22 +0300

    הוצאות צפויות כולל צ'קים

--- netunim-kupa/site/service-worker.js
+++ netunim-kupa/site/service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 const CACHE_PREFIX='kupa-app-shell-';
-const CACHE='kupa-app-shell-esm-7b6782571fba';
+const CACHE='kupa-app-shell-esm-cf488dd386ad';
 const SHELL=[
   './',
   './index.html',
--- netunim-orders/site/service-worker.js
+++ netunim-orders/site/service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 const CACHE_PREFIX='orders-app-shell-';
-const CACHE='orders-app-shell-esm-479431a04274';
+const CACHE='orders-app-shell-esm-569be4ac621b';
 const SHELL=[
   './',
   './index.html',
