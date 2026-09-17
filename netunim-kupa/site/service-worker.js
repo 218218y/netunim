1e0d9404e2e5e3cba466f18bd5b63cb4edd02781
218218y <218yaakov@gmail.com>
2026-09-17 04:02:51 +0300

    כותרות בנק קופה

--- netunim-kupa/site/service-worker.js
+++ netunim-kupa/site/service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 const CACHE_PREFIX='kupa-app-shell-';
-const CACHE='kupa-app-shell-esm-ade39d901078';
+const CACHE='kupa-app-shell-esm-186b7a68ed39';
 const SHELL=[
   './',
   './index.html',
